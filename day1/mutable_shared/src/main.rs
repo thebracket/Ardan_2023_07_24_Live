06e50f826ec5bf736ef428ba24529094566a7d37
Herbert Wolverson <herberticus@gmail.com>
2023-07-24 11:51:12 -0500

    Shared - don't do it this way

--- day1/mutable_shared/src/main.rs
+++ day1/mutable_shared/src/main.rs
@@ -1,3 +1,16 @@
+static mut SHARED: i32 = 0;
+
 fn main() {
-    println!("Hello, world!");
+    std::thread::scope(|scope| {
+        for _ in 0..1000 {
+            scope.spawn(|| unsafe {
+                for _ in 0 .. 1000 {
+                    SHARED += 1;
+                }
+            });
+        }
+    });
+    unsafe {
+        println!("{SHARED}");
+    }
 }
