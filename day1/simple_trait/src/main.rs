8105722c2d74852a32460c6cce6505841bcade8d
Herbert Wolverson <herberticus@gmail.com>
2023-07-24 13:20:27 -0500

    Downcast and any

--- day1/simple_trait/src/main.rs
+++ day1/simple_trait/src/main.rs
@@ -35,6 +35,28 @@ fn get_animal() -> impl Animal {
     Cat
 }
 
+struct Tortoise {
+    shell_size: i32,
+}
+
+impl Animal for Tortoise {
+    fn speak(&self) {
+        println!("What noise does a tortoise make anyway?");
+    }
+}
+
+use std::any::Any;
+
+trait DowncastableAnimal {
+    fn as_any(&self) -> &dyn Any;
+}
+
+impl DowncastableAnimal for Tortoise {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
 fn main() {
     let cat = Cat;
     cat.speak();
@@ -46,5 +68,14 @@ fn main() {
     let animals: Vec<Box<dyn Animal>> = vec![Box::new(Cat), Box::new(Dog)];
     for animal in animals.iter() {
         animal.speak();
+    }
+
+    let more_animals : Vec<Box<dyn DowncastableAnimal>> = vec![Box::new(Tortoise)];
+    for animal in more_animals.iter() {
+        //animal.shell_size <-- Not available
+        if let Some(tortoise) = animal.as_any().downcast_ref::<Tortoise>() {
+            println!("It's a tortoise");
+            tortoise.shell_size;
+        }
     }
 }
