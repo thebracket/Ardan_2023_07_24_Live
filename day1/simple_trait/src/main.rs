c8a3472a1b7c2f6fc400cb60f438b603f420e562
Herbert Wolverson <herberticus@gmail.com>
2023-07-24 12:57:27 -0500

    Define a simple trait

--- day1/simple_trait/src/main.rs
+++ day1/simple_trait/src/main.rs
@@ -1,3 +1,26 @@
+trait Animal {
+    fn speak(&self);
+}
+
+struct Cat;
+
+impl Animal for Cat {
+    fn speak(&self) {
+        println!("Meow");
+    }
+}
+
+struct Dog;
+
+impl Animal for Dog {
+    fn speak(&self) {
+        println!("Woof");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cat = Cat;
+    cat.speak();
+    let dog = Dog;
+    dog.speak();
 }
