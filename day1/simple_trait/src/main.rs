bdf6a513bacc294fd637b0ae0d686939fd70b86c
Herbert Wolverson <herberticus@gmail.com>
2023-07-24 13:07:05 -0500

    Impl requirements

--- day1/simple_trait/src/main.rs
+++ day1/simple_trait/src/main.rs
@@ -1,7 +1,15 @@
 trait Animal {
     fn speak(&self);
+    fn make_noise(&self) {
+        println!("???");
+    }
 }
 
+trait DebuggableAnimal: std::fmt::Debug + Clone {
+    fn speak(&self);
+}
+
+#[derive(Debug)]
 struct Cat;
 
 impl Animal for Cat {
@@ -18,9 +26,20 @@ impl Animal for Dog {
     }
 }
 
+fn speak_twice(animal: &impl Animal) {
+    animal.speak();
+    animal.speak();
+}
+
+fn get_animal() -> impl Animal {
+    Cat
+}
+
 fn main() {
     let cat = Cat;
     cat.speak();
     let dog = Dog;
     dog.speak();
+    speak_twice(&cat);
+    let my_animal = get_animal();
 }
