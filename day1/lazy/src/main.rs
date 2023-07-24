0508e56c2b9c44a243d1ddb7ebda551a07da825a
Herbert Wolverson <herberticus@gmail.com>
2023-07-24 12:38:15 -0500

    Interior mutability

--- day1/lazy/src/main.rs
+++ day1/lazy/src/main.rs
@@ -2,7 +2,7 @@ use std::sync::Mutex;
 use std::sync::Arc;
 
 #[derive(Debug)]
-struct Data (String, String);
+struct Data (Mutex<String>, String);
 
 impl Data {
     fn new() -> Self {
@@ -17,16 +17,16 @@ impl Drop for Data {
 }
 
 fn main() {
-    let shared = Arc::new(Mutex::new(Data::new()));
+    let shared = Arc::new(Data::new());
     std::thread::scope(|scope| {
         for i in 0..10 {
             let my_shared = shared.clone();
             scope.spawn(move || {
-                let mut lock = my_shared.lock().unwrap();
-                lock.0 = format!("{}{i}", lock.0);
+                let mut lock = my_shared.0.lock().unwrap();
+                *lock = format!("{}{i}", lock);
             });
         }
     });
-    let lock = shared.lock().unwrap();
-    println!("{:?}", lock);
+    let lock = shared.0.lock().unwrap();
+    println!("{}, {}", *lock, shared.1);
 }
