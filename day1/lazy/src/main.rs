537dc00a47e1e1df4f6f4e7f80b924c9a3edab15
Herbert Wolverson <herberticus@gmail.com>
2023-07-24 12:32:34 -0500

    Shared state mutex + arc, exterior mutability

--- day1/lazy/src/main.rs
+++ day1/lazy/src/main.rs
@@ -1,7 +1,32 @@
-use once_cell::sync::Lazy;
+use std::sync::Mutex;
+use std::sync::Arc;
 
-static SHARED: Lazy<String> = Lazy::new(|| "Hello".to_string());
+#[derive(Debug)]
+struct Data (String, String);
+
+impl Data {
+    fn new() -> Self {
+        Self("Hello".to_string(), "World".to_string())
+    }
+}
+
+impl Drop for Data {
+    fn drop(&mut self) {
+        println!("Dropping {:?}", self);
+    }
+}
 
 fn main() {
-    println!("{}", *SHARED);
+    let shared = Arc::new(Mutex::new(Data::new()));
+    std::thread::scope(|scope| {
+        for i in 0..10 {
+            let my_shared = shared.clone();
+            scope.spawn(move || {
+                let mut lock = my_shared.lock().unwrap();
+                lock.0 = format!("{}{i}", lock.0);
+            });
+        }
+    });
+    let lock = shared.lock().unwrap();
+    println!("{:?}", lock);
 }
