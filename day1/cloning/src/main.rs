8b9227a40016e3b48d872d27fe1ad4f85dd83bcf
Herbert Wolverson <herberticus@gmail.com>
2023-07-24 10:42:55 -0500

    Clone to replicate data

--- day1/cloning/src/main.rs
+++ day1/cloning/src/main.rs
@@ -1,3 +1,19 @@
+#[derive(Clone)]
+struct Data(String);
+
+impl Drop for Data {
+    fn drop(&mut self) {
+        println!("{} was dropped", self.0);
+    }
+}
+
+fn print(s: Data) {
+    println!("Hello {}", s.0);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let s = Data("Hello".to_string());
+    print(s.clone());
+    print(s);
+    println!("And... we're back");
 }
