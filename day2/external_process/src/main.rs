072989d0d5f8e380dafec8310f3c29ae721c464d
Herbert Wolverson <herberticus@gmail.com>
2023-07-25 13:41:56 -0500

    Receive stdout from process

--- day2/external_process/src/main.rs
+++ day2/external_process/src/main.rs
@@ -3,6 +3,10 @@ use std::process::Command;
 fn main() {
     let result = Command::new("../../target/release/thumbnailer")
         .args(["../photo.jpg", "thumbnail.jpg"])
-        .spawn();
-    println!("{result:?}");
+        .output();
+
+    if let Ok(output) = result {
+        let returned_text = String::from_utf8(output.stdout).unwrap();
+        println!("Process returned: {returned_text}");
+    }
 }
