45c8314d7ef8a93e0e7c414b3e85c7ccd6fa40ca
Herbert Wolverson <herberticus@gmail.com>
2023-07-25 10:50:06 -0500

    MPSC

--- day2/thread_channels/src/main.rs
+++ day2/thread_channels/src/main.rs
@@ -1,3 +1,30 @@
+use std::sync::mpsc;
+
+enum Command {
+    SayHello, Quit, Say(String),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = mpsc::channel::<Command>();
+
+    let handle = std::thread::spawn(move || {
+        while let Ok(command) = rx.recv() {
+            match command {
+                Command::SayHello => println!("Hello"),
+                Command::Say(message) => println!("Hello {message}"),
+                Command::Quit => {
+                    println!("Quitting now");
+                    break;
+                }
+            }
+        }
+    });
+
+    for i in 0 .. 10 {
+        tx.send(Command::SayHello).unwrap();
+        tx.send(Command::Say(format!("{i}"))).unwrap();
+    }
+    println!("Sending Quit");
+    tx.send(Command::Quit).unwrap();
+    handle.join().unwrap();
 }
