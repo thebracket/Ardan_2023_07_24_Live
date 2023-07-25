70e4c043ab155ac3189ec550cdfa0abcbf4d48ac
Herbert Wolverson <herberticus@gmail.com>
2023-07-25 11:30:36 -0500

    Tokio: spawning, joining, joinsets, spawning and yielding

--- day2/hello_tokio/src/main.rs
+++ day2/hello_tokio/src/main.rs
@@ -1,8 +1,35 @@
+use tokio::task::JoinSet;
+
 async fn hello() {
     println!("Hello from async");
 }
 
+async fn double(n: i32) -> i32 {
+    n * 2
+}
+
+async fn ticker() {
+    for i in 0..100 {
+        println!("tick {i}");
+        tokio::task::yield_now().await;
+    }
+}
+
 #[tokio::main]
 async fn main() {
+    tokio::spawn(ticker());
+    tokio::spawn(ticker());
+
     hello().await;
+    let (first, second) = tokio::join!(double(2), double(3));
+    println!("{first}, {second}");
+
+    // Using Tokio JoinSet
+    let mut set = JoinSet::new();
+    for i in 0..10 {
+        set.spawn(double(i));
+    }
+    while let Some(res) = set.join_next().await {
+        println!("{res:?}");
+    }
 }
