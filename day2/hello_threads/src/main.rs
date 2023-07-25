f052c77dd9cde436ad81fb269d5523026ebf2577
Herbert Wolverson <herberticus@gmail.com>
2023-07-25 10:17:18 -0500

    Hello threads

--- day2/hello_threads/src/main.rs
+++ day2/hello_threads/src/main.rs
@@ -1,3 +1,10 @@
+fn hello_thread() {
+    println!("Hello from thread!");
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello from the main thread!");
+
+    let thread_handle = std::thread::spawn(hello_thread);
+    thread_handle.join().unwrap();
 }
