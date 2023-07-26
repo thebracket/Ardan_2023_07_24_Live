639dc04dfd52d3346611c0816317ca4a3485ed15
Herbert Wolverson <herberticus@gmail.com>
2023-07-26 11:34:25 -0500

    Matching the error kind

--- day3/strong_errors/src/main.rs
+++ day3/strong_errors/src/main.rs
@@ -6,7 +6,11 @@ fn main() {
     let contents = std::fs::read_to_string(my_file);
     // Let's just handle the error by printing it out
     match contents {
-        Ok(contents) => println!("File contents: {contents}"),        
-        Err(e) => println!("ERROR: {e:#?}"),
+        Ok(contents) => println!("File contents: {contents}"),
+        Err(e) => match e.kind() {
+            std::io::ErrorKind::NotFound => println!("File (myfile.txt) not found"),
+            std::io::ErrorKind::PermissionDenied => println!("Permission denied"),
+            _ => println!("ERROR: {e:#?}"),
+        },
     }
 }
