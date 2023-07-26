32b708ba91faaa7249884e0017f3265f53df084f
Herbert Wolverson <herberticus@gmail.com>
2023-07-26 12:05:21 -0500

    Define an error the hard way

--- day3/pass_through_thiserror/src/main.rs
+++ day3/pass_through_thiserror/src/main.rs
@@ -1,3 +1,19 @@
+#[derive(Debug, Clone)]
+enum UsersError {
+    NoUsers, TooManyUsers
+}
+
+use std::fmt;
+
+impl fmt::Display for UsersError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            UsersError::NoUsers => write!(f, "no users found"),
+            UsersError::TooManyUsers => write!(f, "too many users found"),
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
