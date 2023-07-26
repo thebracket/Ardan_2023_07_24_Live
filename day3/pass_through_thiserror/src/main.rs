bbebba551060caa581e24938b28a08c9882bc55f
Herbert Wolverson <herberticus@gmail.com>
2023-07-26 12:14:56 -0500

    This error for making your own errors

--- day3/pass_through_thiserror/src/main.rs
+++ day3/pass_through_thiserror/src/main.rs
@@ -1,16 +1,37 @@
-#[derive(Debug, Clone)]
-enum UsersError {
-    NoUsers, TooManyUsers
+use std::path::Path;
+use serde::Deserialize;
+use thiserror::Error;
+
+#[derive(Deserialize)]
+struct User {
+    name: String,
+    password: String,
 }
 
-use std::fmt;
+#[derive(Debug, Error)]
+enum UsersError {
+    #[error("No users were found")]
+    NoUsers,
+    #[error("Too many users were found")]
+    TooManyUsers,
+    #[error("Unable to open users database")]
+    ReadError,
+    #[error("Unable to deserialize json")]
+    JsonError(serde_json::Error),
+}
 
-impl fmt::Display for UsersError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            UsersError::NoUsers => write!(f, "no users found"),
-            UsersError::TooManyUsers => write!(f, "too many users found"),
-        }
+fn work_with_my_error() -> Result<Vec<User>, UsersError> {
+    let my_file = Path::new("users.json");
+    let raw_text = std::fs::read_to_string(my_file)
+        .map_err(|_| UsersError::ReadError)?;
+    let users: Vec<User> = serde_json::from_str(&raw_text)
+        .map_err(|e| UsersError::JsonError(e))?;
+    if users.is_empty() {
+        Err(UsersError::NoUsers)
+    } else if users.len() > 10 {
+        Err(UsersError::TooManyUsers)
+    } else {
+        Ok(users)
     }
 }
 
