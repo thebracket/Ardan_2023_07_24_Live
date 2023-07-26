108ef5ceb110d3d85964f7a35058e630b7d03a63
Herbert Wolverson <herberticus@gmail.com>
2023-07-26 11:22:28 -0500

    Axum dependency/layer type alias

--- day3/databases_axum/src/main.rs
+++ day3/databases_axum/src/main.rs
@@ -1,7 +1,41 @@
+use axum::extract::Path;
 use axum::{routing::get, Router, Json, Extension};
 use serde::{Serialize, Deserialize};
 use sqlx::FromRow;
 use std::net::SocketAddr;
+use std::collections::HashMap;
+
+struct MessageCache {
+    messages: HashMap<i64, HelloJson>,
+}
+
+impl MessageCache {
+    fn new() -> Self {
+        Self {
+            messages: HashMap::new(),
+        }
+    }
+
+    async fn get(&mut self, id: i64, pool: &sqlx::SqlitePool) -> Option<HelloJson> {
+        // Do we have a cached entry?
+        if let Some(msg) = self.messages.get(&id) {
+            // Yes - return it
+            Some(msg.clone())
+        } else {
+            // No - look it up in the database
+            let row = sqlx::query_as::<_, HelloJson>("SELECT * FROM messages WHERE id = ?")
+                .bind(id)
+                .fetch_one(pool)
+                .await;
+            if let Ok(row) = row {
+                self.messages.insert(row.id, row.clone());
+                Some(row)
+            } else {
+                None
+            }
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -15,7 +49,15 @@ async fn main() {
     // Build a router, and add an extension layer containing the database pool
     let app = Router::new()
         .route("/", get(say_hello_json))
-        .layer(Extension(pool));
+        .route("/one/:id", get(get_one))
+        .layer(Extension(pool))
+        .layer(Extension(
+            std::sync::Arc::new(
+                tokio::sync::Mutex::new(
+                    MessageCache::new()
+                )
+            )
+        ));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
@@ -23,7 +65,7 @@ async fn main() {
         .unwrap();
 }
 
-#[derive(Serialize, Deserialize, FromRow)]
+#[derive(Serialize, Deserialize, FromRow, Clone)]
 struct HelloJson {
     id: i64,
     message: String,
@@ -35,4 +77,15 @@ async fn say_hello_json(Extension(pool): Extension<sqlx::SqlitePool>) -> Json<Ve
         .await
         .unwrap();
     Json(result)
+}
+
+type MessagesCache = std::sync::Arc<tokio::sync::Mutex<MessageCache>>;
+
+async fn get_one(
+    Path(id): Path<i64>,
+    Extension(pool): Extension<sqlx::SqlitePool>,
+    Extension(cache): Extension<MessagesCache>,
+) -> Json<HelloJson> {
+    let mut lock = cache.lock().await;
+    Json(lock.get(id, &pool).await.unwrap())
 }
