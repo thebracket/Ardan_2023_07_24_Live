da805ec7d438549ef8b2126c8faefd0849d0a3be
Herbert Wolverson <herberticus@gmail.com>
2023-07-26 10:19:29 -0500

    External processes revisited

--- day3/external_process2/src/main.rs
+++ day3/external_process2/src/main.rs
@@ -1,3 +1,13 @@
+use std::{process, thread, time::Duration};
+
 fn main() {
-    println!("Hello, world!");
+    let mut child = process::Command::new("ping")
+        .arg("8.8.8.8")
+        .arg("-t")
+        .spawn()
+        .expect("Couldn't run 'ping'");
+
+    println!("My PID is: {}", child.id());
+    thread::sleep(Duration::from_secs(5));
+    child.kill().expect("!kill");
 }
