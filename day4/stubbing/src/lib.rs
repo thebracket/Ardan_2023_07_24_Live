ec69afda57fe4d8202cb692ac81b7c869ccc894a
Herbert Wolverson <herberticus@gmail.com>
2023-07-27 10:13:07 -0500

    Conditional compilation for stubbing

--- day4/stubbing/src/lib.rs
+++ day4/stubbing/src/lib.rs
@@ -1,3 +1,12 @@
+#[cfg(not(test))]
+pub fn complex_math() -> i32 {
+    4 * 3 // Let's pretend that's complex
+}
+
+#[cfg(test)]
+pub fn complex_math() -> i32 {
+    12
+}
 
 #[cfg(test)]
 mod test {
@@ -5,6 +14,6 @@ mod test {
     
     #[test]
     fn test() {
-
+        assert_eq!(complex_math(), 12)
     }
 }
