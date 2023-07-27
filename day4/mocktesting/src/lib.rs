3596651bf6e66922a7cf72365cc9abf7bb641d19
Herbert Wolverson <herberticus@gmail.com>
2023-07-27 10:30:39 -0500

    Finish mocking - add property testing

--- day4/mocktesting/src/lib.rs
+++ day4/mocktesting/src/lib.rs
@@ -19,11 +19,24 @@ mod test {
     }
 
     #[test]
-fn test_my_mock_fn() {
-    // Mockall has added a constructor to build a mocked implementation
-    let mut mock = MockMyTrait::new();
-    // Tell the mocked trait that `calculate` is expected to return 42
-    mock.expect_calculate().returning(|x| x + 30);
-    assert_eq!(mock.calculate(12), 42);
-}
+    fn test_my_mock_fn() {
+        // Mockall has added a constructor to build a mocked implementation
+        let mut mock = MockMyTrait::new();
+        // Tell the mocked trait that `calculate` is expected to return 42
+        mock.expect_calculate().returning(|x| x + 30);
+        assert_eq!(mock.calculate(12), 42);
+    }
+
+    use mockall::predicate::*;
+
+    #[test]
+    fn test_my_mock_predicate() {
+        // Mockall has added a constructor to build a mocked implementation
+        let mut mock = MockMyTrait::new();
+        
+        mock.expect_calculate().with(eq(3)).returning(|x| x + 30);
+        mock.expect_calculate().with(eq(4)).returning(|x| x + 31);
+        assert_eq!(mock.calculate(3), 33);
+        assert_eq!(mock.calculate(4), 35);
+    }
 }
