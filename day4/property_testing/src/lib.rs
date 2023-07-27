1904d17993de1906754dd8435c897f2ecea2a1ce
Herbert Wolverson <herberticus@gmail.com>
2023-07-27 10:57:24 -0500

    Faking and property based testing with the quickcheck system

--- day4/property_testing/src/lib.rs
+++ day4/property_testing/src/lib.rs
@@ -15,4 +15,23 @@ mod tests {
         println!("{email}");
         assert!(is_email_valid(&email));
     }
+
+    // Both `Clone` and `Debug` are required by `quickcheck`
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let email = SafeEmail().fake();
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        is_email_valid(&valid_email.0)
+    }
 }
