48aea598703af3944901241c0f29f7a1875415a5
Herbert Wolverson <herberticus@gmail.com>
2023-07-27 10:08:56 -0500

    Doctesting

---
+++ day4/doctesting/src/lib.rs
@@ -0,0 +1,15 @@
+/// Triple a number
+/// 
+/// # Arguments
+/// * `n` - The number to triple
+/// 
+/// # Returns
+/// The tripled number
+/// 
+/// # Examples
+/// ```
+/// assert_eq!(doctesting::triple(2), 6);
+/// ```
+pub fn triple(n: i32) -> i32 {
+    n * 3
+}
